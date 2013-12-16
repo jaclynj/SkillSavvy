9de9cee5bb3daf2181178085b2d828ba9f949608
Jaclyn Jimenez <jaclynejimenez@gmail.com>
2013-12-16 16:40:04 -0500

    fixed resource model issue

---
+++ app/assets/javascripts/models/resource.js
@@ -0,0 +1,16 @@
+App.Models.Resource = Backbone.Model.extend({
+  urlRoot: '/resources',
+  defaults: {
+    added_by:  2,
+    description:  "",
+    price:     "",
+    query:  "",
+    title: "",
+    url:    ""
+  },
+  // attrs is model attributes
+  // options are what's passed from save or set
+  initialize: function(){
+
+  }
+});
