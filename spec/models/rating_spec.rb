309a500069c01069f5fcb4bc799214794c796341
Jaclyn Jimenez <jaclynejimenez@gmail.com>
2013-12-18 15:22:01 -0500

    added some tests

--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -1,5 +1,44 @@
 require 'spec_helper'
 
 describe Rating do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+it "updates in the database" do
+      resource = Resource.new({
+        added_by: 2,
+        description: "How to Solve the Rubix Cube. Invented by Hungarian Erno Rubik in 1974, the Rubik's Cube is a three-dimensional puzzle cube with 54 colored squares, each square one of ...",
+        query: "solve rubix cube",
+        title: "How to Solve the Rubix Cube | eHow",
+        url: "http://www.ehow.com/how_6583718_solve-rubix-cube.html"
+      })
+      resource.save()
+      rating = Rating.new({
+        newbie_rating: 4,
+        overall_rating: 4,
+        resource_id: 1,
+        user_id: 1
+      })
+      rating.save()
+      expect(Rating.all.length).to eq(1)
+    end
+
+    it "can belong to resource" do
+      resource = Resource.new({
+        added_by: 2,
+        description: "How to Solve the Rubix Cube. Invented by Hungarian Erno Rubik in 1974, the Rubik's Cube is a three-dimensional puzzle cube with 54 colored squares, each square one of ...",
+        query: "solve rubix cube",
+        title: "How to Solve the Rubix Cube | eHow",
+        url: "http://www.ehow.com/how_6583718_solve-rubix-cube.html"
+      })
+      resource.save()
+      rating = Rating.new({
+        newbie_rating: 4,
+        overall_rating: 4,
+        resource_id: 1
+      })
+      rating.save()
+      resource.ratings(rating)
+      expect(resource.ratings).to include(rating)
+    end
+
+
 end
--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -1,5 +1,34 @@
 require 'spec_helper'
 
 describe Resource do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it "updates in the database" do
+      r = Resource.new({
+        added_by: 2,
+        description: "How to Solve the Rubix Cube. Invented by Hungarian Erno Rubik in 1974, the Rubik's Cube is a three-dimensional puzzle cube with 54 colored squares, each square one of ...",
+        query: "solve rubix cube",
+        title: "How to Solve the Rubix Cube | eHow",
+        url: "http://www.ehow.com/how_6583718_solve-rubix-cube.html"
+      })
+      r.save()
+      expect(Resource.all.length).to eq(1)
+    end
+    it "created in the database only once" do
+      r1 = Resource.new({
+        added_by: 2,
+        description: "How to Solve the Rubix Cube. Invented by Hungarian Erno Rubik in 1974, the Rubik's Cube is a three-dimensional puzzle cube with 54 colored squares, each square one of ...",
+        query: "solve rubix cube",
+        title: "How to Solve the Rubix Cube | eHow",
+        url: "http://www.ehow.com/how_6583718_solve-rubix-cube.html"
+      })
+      r2 = Resource.new({
+        added_by: 2,
+        description: "How to Solve the Rubix Cube. Invented by Hungarian Erno Rubik in 1974, the Rubik's Cube is a three-dimensional puzzle cube with 54 colored squares, each square one of ...",
+        query: "solve rubix cube",
+        title: "How to Solve the Rubix Cube | eHow",
+        url: "http://www.ehow.com/how_6583718_solve-rubix-cube.html"
+      })
+      r1.save()
+      r2.save()
+      expect(Resource.all.length).to eq(2)
+    end
 end
